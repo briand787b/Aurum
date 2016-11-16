df34202ba182200e7a9412d9bd24e086c26fa60f
Brian D'Ostilio <briand787b@gmail.com>
2016-11-16 13:58:12 -0500

    route to data transfer could not be resolved, set to absolute path now

--- controller.bash
+++ controller.bash
@@ -6,7 +6,7 @@ echo "Now backing up home directory in ~/Backups/servers/${HOSTNAME}.tar.gz"
 tar czvf ~/Backups/servers/${HOSTNAME}.tar.gz ~
 
 echo "Resolving role in backup process... Consulting configuration files: data_transfer.js"
-ENDPOINT=`./data_transfer.js $HOSTNAME $USER`
+ENDPOINT=`/home/brian/Backups/Aurum/data_transfer.js $HOSTNAME $USER`
 
 echo "Sending backups to: ${ENDPOINT}"
-scp ~/Backups/servers/* $ENDPOINT
+scp /home/brian/Backups/servers/* $ENDPOINT
